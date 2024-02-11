28b68531e4d7b10a4f7f3d50e98d40a4357fb6a9
sammilam <124639069+sammilam@users.noreply.github.com>
2024-02-10 21:13:58 -0500

    edited more and began process

--- projects/poster/app.js
+++ projects/poster/app.js
@@ -1,40 +1,40 @@
 console.log("hello")
 
-    const dots = [];
-    const cursor = {
-        x: 0,
-        y: 0,
-    };
+const dots = [];
+const cursor = {
+    x: 0,
+    y: 0,
+};
 
-    for (let i = 0; i < 40; i++) {
-        const dot = document.createElement("div");
-        dot.className = "dot";
-        document.body.appendChild(dot);
-        dots.push(dot);
-    }
+for (let i = 0; i < 40; i++) {
+    const dot = document.createElement("div");
+    dot.className = "dot";
+    document.body.appendChild(dot);
+    dots.push(dot);
+}
 
-    // addded window.scrollY for dots to follow vertical scrolling
-    document.addEventListener("mousemove", (e) => {
-        cursor.x = e.clientX;
-        cursor.y = e.clientY + window.scrollY; 
+// addded window.scrollY for dots to follow vertical scrolling
+document.addEventListener("mousemove", (e) => {
+    cursor.x = e.clientX;
+    cursor.y = e.clientY + window.scrollY;
+});
+
+function draw() {
+    let x = cursor.x;
+    let y = cursor.y;
+
+    dots.forEach((dot, index) => {
+        const nextDot = dots[index + 1] || dots[0];
+
+        dot.style.left = x + "px";
+        dot.style.top = y + "px";
+
+        x += (nextDot.offsetLeft - dot.offsetLeft) * 0.5;
+        y += (nextDot.offsetTop - dot.offsetTop) * 0.5;
     });
+}
 
-    function draw() {
-        let x = cursor.x;
-        let y = cursor.y;
-
-        dots.forEach((dot, index) => {
-            const nextDot = dots[index + 1] || dots[0];
-
-            dot.style.left = x + "px";
-            dot.style.top = y + "px";
-
-            x += (nextDot.offsetLeft - dot.offsetLeft) * 0.5;
-            y += (nextDot.offsetTop - dot.offsetTop) * 0.5;
-        });
-    }
-
-    setInterval(draw, 10);
+setInterval(draw, 10);
 
 // tutorial from: https://www.youtube.com/watch?v=g33XyH8BDuM&ab_channel=SnippetsCode
 
@@ -58,14 +58,14 @@ chars.forEach(char => {
         char.style.color = getRandomColor();
     });
 
-// mouse off = revert back to white
+    // mouse off = revert back to white
     char.addEventListener('mouseout', () => {
         char.style.color = 'white';
     });
 });
 
 function getRandomColor() {
-    const colors = ['#00FFFF', '#800080', '#FF1493', '#00FF00']; 
+    const colors = ['#00FFFF', '#800080', '#FF1493', '#00FF00'];
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
