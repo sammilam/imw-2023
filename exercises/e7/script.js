d07eaa98a537cba3c1324da527a47f29aa293fc5
sammilam <sammi-lam@hotmail.com>
2024-02-27 20:40:34 -0500

    updated e7

---
+++ exercises/e7/script.js
@@ -0,0 +1,25 @@
+console.log("hello");
+
+$('body').css('background-color', 'white');
+
+let text = 'hello';
+
+$('.background').hide();
+
+for (let i = 0; i < 500; i++) {
+    let block = `<div style="width: ${i}px; height: 100vw; background-color: hsl(${i}, 100%, 50%)"> </div>`;
+    $('.foreground').append(block);
+}
+
+
+$('div').each(function () {
+    $(this).on('mouseenter', function(){
+        $(this).addClass('no-width')
+    })
+})
+
+$('body').on("keypress", function(){
+console.log ("key pressed");
+$('div').removeClass('no-width')
+})
+
