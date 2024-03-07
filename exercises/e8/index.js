390ea6e6b157493e367b650ccc4024df9001cd77
sammilam <124639069+sammilam@users.noreply.github.com>
2024-03-07 16:31:56 -0500

    added comments

--- exercises/e8/index.js
+++ exercises/e8/index.js
@@ -1,18 +1,23 @@
 function fetchImage() {
     fetch('https://dog.ceo/api/breeds/image/random')
+        // Converts the response to JSON format
         .then(response => response.json())
+        // Handles the data received from the API
         .then(data => {
-            const img = document.getElementById("img-api")
-            img.src = data.message
+            // Gets the image element by its ID from the HTML
+            const img = document.getElementById("img-api");
+            // Sets source of image element to URL of fetched image
+            img.src = data.message;
         })
+        // Catches any errors that occur during the fetch process
         .catch(function (error) {
             console.log(error);
         });
 }
 
-
+// Gets the button element from the HTML with the ID 'fetch-image-button'
 const button = document.getElementById('fetch-image-button');
+// event listener to the button which triggers the fetchImage function when clicked
 button.addEventListener('click', fetchImage);
-
 
 // API from https://dog.ceo/dog-api/
