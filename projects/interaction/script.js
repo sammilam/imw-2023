00b2ad15ddc15d97c09625b82270cade24f704b0
sammilam <124639069+sammilam@users.noreply.github.com>
2024-03-26 15:17:40 -0400

    process

--- projects/interaction/script.js
+++ projects/interaction/script.js
@@ -1,5 +1,7 @@
 console.log("hello");
 
+
+// function that when yellow is clicked on create more waves
 $(document).ready(function () {
     $(".yellowCircle1").on("click", function () {
         var $newWave = $("<div>").addClass("waves");
@@ -7,12 +9,13 @@ $(document).ready(function () {
 
         setTimeout(function () {
             $newWave.remove();
-        }, 3000); // 2000 milliseconds = 2 seconds
+        }, 3000);
 
         console.log("clicked");
     });
 });
 
+// same as first function but for the other side
 $(document).ready(function () {
     $(".yellowCircle2").on("click", function () {
         var $newWave = $("<div>").addClass("waves");
@@ -20,31 +23,20 @@ $(document).ready(function () {
 
         setTimeout(function () {
             $newWave.remove();
-        }, 3000); // 2000 milliseconds = 2 seconds
+        }, 3000);
 
         console.log("clicked");
     });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var disc = document.getElementById('disc');
-    var audio = document.getElementById('audio');
-    var currentSong = 0;
-    var songs = ['Fret.mp3', 'Girl on Top.mp3', 'Woodshedder.mp3'];
 
-    disc.addEventListener('click', function () {
-        currentSong = (currentSong + 1) % songs.length; // Increment current song index
-        audio.src = songs[currentSong]; // Set the source of the audio element to the next song
-        audio.play(); // Play the new song
-    });
-});
-
-
+// Plays music and display its name
 document.addEventListener('DOMContentLoaded', function () {
     var disc = document.getElementById('disc');
     var audio = document.getElementById('audio');
     var songNameElement = document.getElementById('songName');
     var currentSong = 0;
+    // song in an array and aligns along with the names
     var songs = ['Fret.mp3', 'Girl on Top.mp3', 'Woodshedder.mp3'];
     var songNames = ['Dont Fret by Quincas Moreira', 'Girl on Top by Amy Lynn & The Honey Men', 'Woodshedder Quincas Moreira'];
 
@@ -52,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
         currentSong = (currentSong + 1) % songs.length;
         audio.src = songs[currentSong];
         audio.play();
-        displaySongName(); // Call the function to update the displayed song name\
+        displaySongName(); // Call the function to update the displayed song name
     });
 
     function displaySongName() {
